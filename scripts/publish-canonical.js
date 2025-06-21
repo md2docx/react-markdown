e7ac3e6cf958593ebe0b1da184a97b75e7590b86
Mayank <mayank.srmu@gmail.com>
2025-06-21 21:48:13 +0530

    style: Refactor formatting of package publishing script

--- scripts/publish-canonical.js
+++ scripts/publish-canonical.js
@@ -1,14 +1,11 @@
 const { execSync } = require("child_process");
 
 // Publish canonical packages
-[
-  "@m2d/jsx",
-  "@md2docx/jsx",
-  "@mdast2docx/jsx",
-  "@m2d/react-markdown",
-  "@md2docx/react-markdown",
-  "mdx-render",
-].forEach(pkg => {
-  execSync(`sed -i -e "s/name.*/name\\": \\"${pkg.replace(/\//g, "\\\\/")}\\",/" lib/package.json`);
-  execSync("cd lib && npm publish --provenance --access public");
-});
+["@m2d/jsx", "@md2docx/jsx", "@mdast2docx/jsx", "@md2docx/react-markdown", "mdx-render"].forEach(
+  pkg => {
+    execSync(
+      `sed -i -e "s/name.*/name\\": \\"${pkg.replace(/\//g, "\\\\/")}\\",/" lib/package.json`,
+    );
+    execSync("cd lib && npm publish --provenance --access public");
+  },
+);
